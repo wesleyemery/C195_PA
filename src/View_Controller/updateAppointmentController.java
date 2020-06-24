72630245ce25586bdb5a9416bcebc4382b9b6999
we3kb.we <we3kb.we@gmail.com>
2020-06-24 16:44:39 -0500

    added a bunch of stuff

--- src/View_Controller/updateAppointmentController.java
+++ src/View_Controller/updateAppointmentController.java
@@ -1,11 +1,16 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class updateAppointmentController {
     @FXML
@@ -46,6 +51,48 @@ public class updateAppointmentController {
 
     @FXML
     private DatePicker datePicker1;
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button updateCustomerBtn;
+
+    @FXML
+    void cancelHandler(ActionEvent event) {
+        String message = "Are you sure you want to cancel?";
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("ALERT: Cancel");
+        alert.setHeaderText("Confirm");
+        alert.setContentText(message);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            backToMain(event);
+        }
+    }
+
+    private void backToMain(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+            MainScreenController controller = new MainScreenController();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            loader.setController(controller);
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void updateCustomerHandler(ActionEvent event) {
+
+    }
+
 
 
 }
--- src/View_Controller/updateCustomerController.java
+++ src/View_Controller/updateCustomerController.java
@@ -2,12 +2,20 @@ package View_Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-public class updateCustomerController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class updateCustomerController implements Initializable {
     @FXML
     private Label customerNameLabel;
 
@@ -31,11 +39,41 @@ public class updateCustomerController {
 
     @FXML
     void cancelHandler(ActionEvent event) {
-
+        String message = "Are you sure you want to cancel?";
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("ALERT: Cancel");
+        alert.setHeaderText("Confirm");
+        alert.setContentText(message);
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            backToMain(event);
+        }
     }
 
     @FXML
     void updateCustomerHandler(ActionEvent event) {
+
+    }
+    private void backToMain(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+            MainScreenController controller = new MainScreenController();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            loader.setController(controller);
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
